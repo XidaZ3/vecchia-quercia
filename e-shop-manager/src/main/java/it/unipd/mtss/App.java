feed574da3db336975f589bceba8f022b80db4be
Danilo Stojkovic <danilostojkovic749@gmail.com>
2022-05-17 19:02:18 +0200

    creazione progetto maven e build lifecycle

---
+++ e-shop-manager/src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////
+// XIDA CHEN 1217780
+// DANILO STOJKOVIC 1222399
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
